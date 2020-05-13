c252f1a010de65eee5cd47918488fe889e843925
Bruno Moraes <bruno_bmoraes@hotmail.com>
2020-05-13 19:09:04 -0300

    arquivos gerais

---
+++ 12 - diretivas/diretivas-exercicios/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h1>Diretivas</h1>
+    <hr />
+    <p v-text="'usando diretiva v-text'"></p>
+    <p v-html="'usando diretiva <b>v-html</b>'"></p>
+    <hr />
+    <p v-destaque>usando diretiva personalizada</p>
+    <p v-destaque="'red'">usando diretiva personalizada</p>
+    <p v-destaque="cor">usando diretiva personalizada</p>
+    <p v-destaque:fundo.atrasar="'blue'">teste</p>
+
+    <hr />
+    <p v-destaque-local>usando diretiva personalizada</p>
+    <p v-destaque-local="'red'">usando diretiva personalizada</p>
+    <p v-destaque-local="cor">usando diretiva personalizada</p>
+    <p v-destaque-local:fundo.atrasar="'blue'">teste</p>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    "destaque-local": {
+      bind(el, binding) {
+        // el.style.backgroundColor = "lightgreen";
+        //pode passar argumento
+        let atraso = 0;
+        if (binding.modifiers["atrasar"]) atraso = 3000;
+
+        setTimeout(() => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, atraso);
+      }
+    }
+  },
+  data() {
+    return {
+      cor: "blue"
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>
---
+++ 13-filtro e mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>Filtros & Mixins</h1>
+    <p>{{ cpf | cpfMask }}</p>
+    <p>{{ cpf | cpfMask |inverter}}</p>
+    <p>{{ 'batata frita'|cpfMask |inverter }}</p>
+    <p>{{ 'batata frita'|inverter }}</p>
+    <input type="text" :value="cpf |cpfMask " />
+    <Frutas />
+  </div>
+</template>
+
+<script>
+import Frutas from "./Frutas.vue";
+
+export default {
+  components: { Frutas },
+
+  filters: {
+    cpfMask(valor) {
+      const arr = valor.split("");
+      arr.splice(3, 0, ".");
+      arr.splice(7, 0, ".");
+      arr.splice(11, 0, "-");
+      return arr.join("");
+    }
+  },
+  data() {
+    return {
+      cpf: "12345678901"
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>
